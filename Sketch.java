f102c3ba35912b218380ba0a3cc07a1122a1333b
jenusan <jenusan.yogarajah23@ycdsbk12.ca>
2022-02-22 11:35:54 -0500

    final

--- Sketch.java
+++ Sketch.java
@@ -16,7 +16,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(51, 204, 255);
   }
 
   /**
@@ -24,12 +24,52 @@ public class Sketch extends PApplet {
    */
   public void draw() {
 	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	// sample code, delete this stuff\
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    //sun
+    fill(255,255,0);
+    ellipse(400,0,200,200);
+
+    //foreground
+    fill(0,255,0);
+    rect(0, 300, 400,100);
+
+    //purple flower
+    line(200, 300, 200, 225);
+
+    fill(102,0,153);
+    ellipse(200,225,60,20);
+
+    fill(102,0,153);
+    ellipse(200,225,20,60);
+
+    fill(0,255,0);
+    ellipse(200,225,20,20);
+
+    //red flower
+    line(300, 300, 300, 225);
+
+    fill(255,0,0);
+    ellipse(300,225,60,20);
+
+    fill(255,0,0);
+    ellipse(300,225,20,60);
+
+    fill(0,255,0);
+    ellipse(300,225,20,20);
+
+    //blue flower
+    line(100, 300, 100, 225);
+
+    fill(0,0,255);
+    ellipse(100,225,60,20);
+
+    fill(0,0,255);
+    ellipse(100,225,20,60);
+
+    fill(0,255,0);
+    ellipse(100,225,20,20);
+
   }
   
   // define other methods down here.
